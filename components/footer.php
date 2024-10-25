cdd4c8adfa4d6e1afefb45df4b3fb9b9f7e51422
SAVARISACHIN555 <savarisachin55@gmail.com>
2024-10-25 23:41:13 +0530

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,41 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>Quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> Shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>Extra links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> Login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> Register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> Cart</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>Contact Us.</h3>
+         <a href="tel: +91 9361179455"><i class="fas fa-phone"></i> +91 9361179455</a>
+         <a href="tel:+91 6385850651"><i class="fas fa-phone"></i> +91 6385850651</a>
+         <a href="mailto:yogesas@karunya.edu.in"><i class="fas fa-envelope"></i> yogesas@karunya.edu.in</a>
+         <a href="https://maps.app.goo.gl/UHBBWWjVDmGXrwD58"><i class="fas fa-map-marker-alt"></i> Tune Traders </a>
+      </div>
+
+      <div class="box">
+         <h3>Follow Us</h3>
+         <a href="https://www.facebook.com/share/SKBhKHZ35SGJLSfz/?mibextid=LQQJ4d" target="_blank"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="https://twitter.com/yogesa07" target="_blank"><i class="fab fa-twitter"></i>Twitter</a>
+         <a href="https://www.instagram.com/__yogesa__/" target="_blank"><i class="fab fa-instagram"></i>Instagram</a>
+         <a href="https://www.linkedin.com/in/yogesa" target="_blank"><i class="fab fa-linkedin"></i>Linkedin</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Tune Traders</span> | all rights reserved!</div>
+
+</footer>
